ad5ebc32052651be96d9c1db9856297dcab440ba
Ilhamhibatul <ilhamhibatul9@gmail.com>
2024-06-14 18:01:19 +0700

    sidebar fix

--- frontend/src/components/Shared/Sidebar/Sidebar.js
+++ frontend/src/components/Shared/Sidebar/Sidebar.js
@@ -5,10 +5,8 @@ import Collapsible from 'react-collapsible';
 
 const Sidebar = ({ show, handleClose }) => {
    const Beranda = <NavLink to="/home">Beranda</NavLink>
-   const Brands = <NavLink to="/services">Brands </NavLink>
-   const Doctors = <NavLink to="/doctorstwo">Dokter</NavLink>
    const Shop = <NavLink to="/shop">Shop</NavLink>
-   const Pages = <a href="#">Pages</a>
+   const Halaman = <a href="#">Halaman</a>
    return (
       <>
 
@@ -26,16 +24,6 @@ const Sidebar = ({ show, handleClose }) => {
                      </ul>
                   </Collapsible>
 
-                  <Collapsible trigger={Brands} triggerTagName="div"
-                     triggerOpenedClassName="icon_close" triggerClassName="iconAdd" open={false}>
-                  </Collapsible>
-
-                  <Collapsible trigger={Doctors} triggerTagName="div"
-                     triggerOpenedClassName="icon_close" triggerClassName="iconAdd" open={false}>
-                     <ul className="sidebar_sub_menu text-white mt-3">
-                        
-                     </ul>
-                  </Collapsible>
 
                   <Collapsible trigger={Shop} triggerTagName="div"
                      triggerOpenedClassName="icon_close" triggerClassName="iconAdd" open={false}>
@@ -44,7 +32,7 @@ const Sidebar = ({ show, handleClose }) => {
                      </ul>
                   </Collapsible>
 
-                  <Collapsible trigger={Pages} triggerTagName="div"
+                  <Collapsible trigger={Halaman} triggerTagName="div"
                      triggerOpenedClassName="icon_close" triggerClassName="iconAdd" open={false}>
                      <ul className="sidebar_sub_menu text-white mt-3">
                         <li><NavLink to="/about">Tentang</NavLink></li>
